059914e4fd01c03f559c047706a06afa00ec6a99
Progrema Studio <progrema.studio@gmail.com>
2018-07-28 04:38:01 +0700

    Add test from coursera

--- src/main/java/com/progrema/algo/part2week2/TestClient.java
+++ src/main/java/com/progrema/algo/part2week2/TestClient.java
@@ -8,6 +8,7 @@ import com.progrema.algo.UnitTest;
 import com.progrema.algo.part2week2.SeamCarver.Pixel;
 
 import edu.princeton.cs.algs4.Picture;
+import edu.princeton.cs.algs4.StdOut;
 import java.lang.Math;
 
 public class TestClient extends UnitTest {
@@ -15,7 +16,8 @@ public class TestClient extends UnitTest {
     public static void main(String[] args) {        
         // testPicture(args);
         // testEnergy(args);
-        testPixelsAdj(args);
+        // testPixelsAdj(args);
+        testPrintSeam(args);
     }
 
     /**
@@ -135,5 +137,61 @@ public class TestClient extends UnitTest {
         }            
 
         endTest(new Object(){}.getClass().getEnclosingMethod().getName()); 
+    }
+
+    private static final boolean HORIZONTAL   = true;
+    private static final boolean VERTICAL     = false;
+
+    private static void printSeam(SeamCarver carver, int[] seam, boolean direction) {
+        double totalSeamEnergy = 0.0;
+
+        for (int row = 0; row < carver.height(); row++) {
+            for (int col = 0; col < carver.width(); col++) {
+                double energy = carver.energy(col, row);
+                String marker = " ";
+                if ((direction == HORIZONTAL && row == seam[col]) ||
+                    (direction == VERTICAL   && col == seam[row])) {
+                    marker = "*";
+                    totalSeamEnergy += energy;
+                }
+                StdOut.printf("%7.2f%s ", energy, marker);
+            }
+            StdOut.println();
+        }                
+        // StdOut.println();
+        StdOut.printf("Total energy = %f\n", totalSeamEnergy);
+        StdOut.println();
+        StdOut.println();
+    }
+
+    /**
+     * Test from Coursera
+     * 
+     * $ gradle run -Pargs="input/part2week2/6x5.png"
+     */
+    public static void testPrintSeam(String[] args) {
+
+        Picture picture = new Picture(args[0]);
+        StdOut.printf("%s (%d-by-%d image)\n", args[0], picture.width(), picture.height());
+        StdOut.println();
+        StdOut.println("The table gives the dual-gradient energies of each pixel.");
+        StdOut.println("The asterisks denote a minimum energy vertical or horizontal seam.");
+        StdOut.println();
+
+        SeamCarver carver = new SeamCarver(picture);
+        
+        StdOut.printf("Vertical seam: { ");
+        int[] verticalSeam = carver.findVerticalSeam();
+        for (int x : verticalSeam)
+            StdOut.print(x + " ");
+        StdOut.println("}");
+        printSeam(carver, verticalSeam, VERTICAL);
+
+        // StdOut.printf("Horizontal seam: { ");
+        // int[] horizontalSeam = carver.findHorizontalSeam();
+        // for (int y : horizontalSeam)
+        //     StdOut.print(y + " ");
+        // StdOut.println("}");
+        // printSeam(carver, horizontalSeam, HORIZONTAL);
     }
 }
